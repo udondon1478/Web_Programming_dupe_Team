5f48808894855e5d2175e7d0a672154d5bfad4d3
udondon1478 <udon1478don@gmail.com>
2024-01-23 15:54:24 +0900

    トップページ等を追加
    キー名が違うとの不具合が発生中

--- db_setup.php
+++ db_setup.php
@@ -31,13 +31,13 @@ echo "user_tbを作成しました<br>";
 
 
 //テスト用管理者ユーザの登録
-$sql = "INSERT INTO `user_tb` (`email`,`password`,`name`,`is_admin`)
+$sql = "INSERT INTO `user_tb` (`email`,`password`,`username`,`is_admin`)
 VALUES('x22004xx@aitech.ac.jp','".password_hash('webphp',PASSWORD_DEFAULT)."','あん',TRUE);";
 $dbh->exec($sql);//SQLの実行
 echo "テスト用管理者ユーザを登録しました<br>";
 
 //テスト用一般ユーザの登録
-$sql = "INSERT INTO `user_tb` (`email`,`password`,`name`,`is_admin`)
+$sql = "INSERT INTO `user_tb` (`email`,`password`,`username`,`is_admin`)
 VALUES('x22999xx@aitech.ac.jp','".password_hash('webphp',PASSWORD_DEFAULT)."','ゆう',FALSE);";
 $dbh->exec($sql);//SQLの実行
 echo "テスト用一般ユーザを登録しました<br>";
@@ -65,7 +65,7 @@ $dbh->exec($sql);//SQLの実行
 echo "post_tbを作成しました<br>";
 
 //返信テーブルの作成
-$sql = "CREATE TABLE `reply_tb`(`id` INT AUTO_INCREMENT PRIMARY KEY,`post_id` INT NOT NULL,`user_id` INT NOT NULL,`name` VARCHAR(255) NOT NULL,`content` TEXT,`image_path` VARCHAR(255),`created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP),FOREIGN KEY (`post_id`) REFERENCES `post_tb`(`id`),FOREIGN KEY (`user_id`) REFERENCES `user_tb`(`id`));";
+$sql = "CREATE TABLE `reply_tb`(`id` INT AUTO_INCREMENT PRIMARY KEY,`post_id` INT NOT NULL,`user_id` INT NOT NULL,`name` VARCHAR(255) NOT NULL,`content` TEXT,`image_path` VARCHAR(255),`created_at` TIMESTAMP DEFAULT CURRENT_TIMESTAMP,FOREIGN KEY (`post_id`) REFERENCES `post_tb`(`id`),FOREIGN KEY (`user_id`) REFERENCES `user_tb`(`id`));";
 $dbh->exec($sql);//SQLの実行
 echo "reply_tbを作成しました<br>";
 
